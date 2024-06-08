f5014a064453ca7a90d3626596e78930e06629ca
Muhammad Yasin <166158270+Muhammad313313@users.noreply.github.com>
2024-06-08 23:20:40 +0500

    Add files via upload

---
+++ Complaint Portal/includes/Footer.php
@@ -0,0 +1,104 @@
+
+<style>
+            /* Footer styles */
+            .footer {
+            background-color: rgb(0, 102, 51);
+            color: #fff;
+            font-size: x-small;
+            text-align: center;
+            padding-top: 20px;
+            width: 100%;
+            position: static;
+            bottom: 0;
+            margin-top: 20px;
+        }
+
+        .footer-content {
+            display: flex;
+            flex-wrap: wrap;
+            font-size: x-small;
+            justify-content: space-around;
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+
+        .footer-section {
+            flex: 1;
+            padding: 0 20px;
+        }
+
+        .footer-section h3 {
+            color: #fff;
+            font-size: 18px;
+            margin-bottom: 20px;
+            text-transform: uppercase;
+        }
+
+        .footer-section ul {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .footer-section ul li {
+            margin-bottom: 10px;
+        }
+
+        .footer-section a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        .footer-section a:hover {
+            text-decoration: underline;
+        }
+
+        .footer-bottom {
+            background-color: #222;
+            text-align: center;
+            padding: 5px 0;
+        }
+
+        .footer-bottom p {
+            margin: 0;
+            color: #fff;
+            font-size: 14px;
+        }
+
+        .footer-bottom a {
+            color: #fff;
+            text-decoration: none;
+            font-weight: bold;
+        }
+
+        .footer-bottom a:hover {
+            text-decoration: underline;
+        }
+</style>
+
+
+<div class="footer">
+      <div class="footer-content">
+        <div class="footer-section">
+          <h6>About Us</h6>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget odio nec ipsum scelerisque ultricies.</p>
+        </div>
+        <div class="footer-section">
+          <h6>Quick Links</h6>
+          <ul>
+            <li><a href="#">New Link 1</a></li>
+            <li><a href="#">New Link 2</a></li>
+            <li><a href="#">New Link 3</a></li>
+          </ul>
+        </div>
+        <div class="footer-section">
+          <h6>Contact Us</h6>
+          <p>123 Street Name, City, Country</p>
+          <p>Email: info@example.com</p>
+          <p>Phone: +1 234 567 890</p>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <p style="font-size: xx-small;">&copy; 2024 UOM Complaint Portal. All Rights Reserved. Designed by <a href="https://www.yourwebsite.com" target="_blank">Engr.Muhammad Yasin & Engr.Syed Adil</a></p>
+      </div>
+    </div>
